34443a9712da655a359a319e8037e073a669b5da
topi768 <topi768@inbox.ru>
2024-07-21 01:07:02 +1000

    add turn texture on player

---
+++ Building_D_Escape/Assets/Common/Scripts/PlayerControler.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private bool isMoving;
+    private Vector2 input;
+
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+
+        transform.position = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), -1);
+    }
+    
+    private void Update() {
+        if (!isMoving) {
+
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            Debug.Log("X: " + input.x);
+            Debug.Log("Y: " + input.y);
+
+            if (input.x != 0) input.y = 0;
+
+
+            if(input !=Vector2.zero) {
+
+                animator.SetFloat("MoveX", input.x);
+                animator.SetFloat("MoveY", input.y);
+
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+
+            }
+
+        }
+    }
+    IEnumerator Move(Vector3 targetPos) {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos,moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+}
--- Building_D_Escape/Assets/Scripts/PlayerControler.cs
+++ Building_D_Escape/Assets/Scripts/PlayerControler.cs
@@ -1,46 +1,61 @@
+
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour {
-
-    public float moveSpead;
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
 
     private bool isMoving;
-    private Vector2 input;
-    void Start()
-    {
 
-        transform.position = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), -1);
+    private Vector2 input;
+
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
     }
-    
-    private void Update() {
-        if (!isMoving) {
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
             if (input.x != 0) input.y = 0;
 
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("MoveX", input.x);
+                animator.SetFloat("MoveY", input.y);
 
-            if(input !=Vector2.zero) {
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
                 StartCoroutine(Move(targetPos));
-
             }
 
         }
     }
-    IEnumerator Move(Vector3 targetPos) {
+
+    IEnumerator Move(Vector3 targetPos)
+    {
         isMoving = true;
+
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos,moveSpead * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
             yield return null;
         }
         transform.position = targetPos;
+
         isMoving = false;
     }
+
+
+
 }
