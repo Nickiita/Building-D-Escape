28763f7857e9319916284f674b41e299b403e71a
topi768 <topi768@inbox.ru>
2024-07-21 21:44:26 +1000

    fix merge conflict

--- Building_D_Escape/Assets/Common/Scripts/PlayerControler.cs
+++ Building_D_Escape/Assets/Common/Scripts/PlayerControler.cs
@@ -5,11 +5,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
-<<<<<<< HEAD
     private bool isMoving;
-=======
-
->>>>>>> e6bd1343efee0cdb75132ef3ae9946c222f3d132
     private Vector2 movementInput;
     private Animator animator;
 
@@ -33,19 +29,13 @@ public class PlayerController : MonoBehaviour
 
         if (movementInput != Vector2.zero)
         {
-<<<<<<< HEAD
             isMoving = true;
-=======
->>>>>>> e6bd1343efee0cdb75132ef3ae9946c222f3d132
             animator.SetFloat("MoveX", movementInput.x);
             animator.SetFloat("MoveY", movementInput.y);
 
             Vector2 targetPosition = (Vector2)transform.position + movementInput * moveSpeed * Time.deltaTime;
             transform.position = new Vector3(targetPosition.x, targetPosition.y, transform.position.z);
-<<<<<<< HEAD
             isMoving = false;
-=======
->>>>>>> e6bd1343efee0cdb75132ef3ae9946c222f3d132
         }
     }
     // animator.SetBool("isMoving", isMoving)
