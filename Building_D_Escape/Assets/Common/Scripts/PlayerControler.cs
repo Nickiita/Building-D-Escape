224e4dfd1f632d387cd7017c1c4bd62b8f5c3b6f
NotTheStas <113777167+NotTheStas@users.noreply.github.com>
2024-07-25 15:12:43 +1000

    collisions

--- Building_D_Escape/Assets/Common/Scripts/PlayerControler.cs
+++ Building_D_Escape/Assets/Common/Scripts/PlayerControler.cs
@@ -3,68 +3,74 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-
     public float moveSpeed;
     public bool isMoving;
 
     private Vector2 movementInput;
     private Animator animator;
+    private BoxCollider2D boxCollider;
 
     public LayerMask solidObjectsLayer;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Start()
     {
-        transform.position = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        transform.position = new Vector2(0, 0);
     }
 
     private void Update()
     {
         movementInput.x = Input.GetAxisRaw("Horizontal");
         movementInput.y = Input.GetAxisRaw("Vertical");
-        // Debug.Log("X: " + movementInput.x);
-        // Debug.Log("Y: " + movementInput.y);
+
         if (movementInput != Vector2.zero)
         {
+            Move();
+        }
 
-            Vector2 targetPos = (Vector2)transform.position + movementInput * moveSpeed * Time.deltaTime;
-            // animator.SetBool("isMoving", true);
-            // animator.SetFloat("MoveX", movementInput.x);
-            // animator.SetFloat("MoveY", movementInput.y);
+        animator.SetBool("isMoving", movementInput != Vector2.zero);
+        animator.SetFloat("MoveX", movementInput.x);
+        animator.SetFloat("MoveY", movementInput.y);
+    }
 
-            // transform.position = new Vector3(targetPos.x, targetPos.y, transform.position.z);
+    private void Move()
+    {
+        Vector3 targetPos = transform.position + (Vector3)movementInput * moveSpeed * Time.deltaTime;
 
+        if (IsWalkable(targetPos))
+        {
+            transform.position = targetPos;
+        }
+        else
+        {
+            Vector3 checkPosX = new Vector3(targetPos.x, transform.position.y, transform.position.z);
+            Vector3 checkPosY = new Vector3(transform.position.x, targetPos.y, transform.position.z);
 
-                    // collision
-            Debug.Log(IsWalkable(targetPos));
-            if (IsWalkable(targetPos)) {
-                
-
-            animator.SetBool("isMoving", true);
-            animator.SetFloat("MoveX", movementInput.x);
-            animator.SetFloat("MoveY", movementInput.y);
-
-            transform.position = new Vector3(targetPos.x, targetPos.y, transform.position.z);
-
+            if (IsWalkable(checkPosX))
+            {
+                transform.position = checkPosX;
             }
-            
-        }else {
-            animator.SetBool("isMoving", false);
-        }
-          
-
-    }
-
-    private bool IsWalkable(Vector3 targetPos) {
-        if(Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null ) {
-            return false;
-
-        }else {
-            return true;
+            else if (IsWalkable(checkPosY))
+            {
+                transform.position = checkPosY;
+            }
         }
     }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        Vector2 size = boxCollider.size;
+        Vector2 offset = boxCollider.offset;
+        Vector2 center = (Vector2)targetPos + offset;
+
+        Collider2D hit = Physics2D.OverlapBox(center, size, 0f, solidObjectsLayer);
+        return hit == null;
+    }
+
 }
