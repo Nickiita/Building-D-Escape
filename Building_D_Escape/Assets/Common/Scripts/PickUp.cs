5b68506cde2ea5cdf21e2919bdd6993c5089080f
Nickii <51742504+Nickiita@users.noreply.github.com>
2024-07-28 19:41:13 +1000

    Revert "Merge branch 'main' of https://github.com/Nickiita/Building-D-Escape"
    This reverts commit 8ca62dcd2c3efb170b499fe00f12a4b3dc448ef7, reversing
    changes made to 090c9482c5b7ce3d081bfb737c8981a145994a35.

--- Building_D_Escape/Assets/Common/Scripts/PickUp.cs
+++ Building_D_Escape/Assets/Common/Scripts/PickUp.cs
@@ -4,40 +4,32 @@ using UnityEngine.UI;
 public class DisappearOnApproach : MonoBehaviour
 {
     public Transform player; // Ссылка на трансформ персонажа
-    public float interactDistance = 5f; // Расстояние, на котором можно взаимодействовать с объектом
-    public Image[] uiSlots; // Массив UI Image для слотов
-    private bool canInteract = false;
-    private static int currentSlotIndex = 0; // Текущий индекс слота для отображения предмета
+    public float disappearDistance = 5f; // Расстояние, на котором объект исчезает
+    public Image uiImage; // Ссылка на UI Image для отображения спрайта
 
     void Start()
     {
-        // Отключаем все слоты по умолчанию
-        foreach (Image slot in uiSlots)
+        if (uiImage != null)
         {
-            slot.gameObject.SetActive(false);
+            uiImage.gameObject.SetActive(false); // Отключаем UI Image по умолчанию
         }
     }
 
     void Update()
     {
         float distance = Vector3.Distance(transform.position, player.position);
-        canInteract = distance < interactDistance; // Проверка на расстояние для взаимодействия
-    }
 
-    void OnMouseDown()
-    {
-        if (canInteract && currentSlotIndex < uiSlots.Length)
+        if (distance < disappearDistance)
         {
             gameObject.SetActive(false); // Отключаем объект
 
-            if (uiSlots[currentSlotIndex] != null)
+            if (uiImage != null)
             {
                 SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
                 if (spriteRenderer != null)
                 {
-                    uiSlots[currentSlotIndex].sprite = spriteRenderer.sprite; // Устанавливаем спрайт
-                    uiSlots[currentSlotIndex].gameObject.SetActive(true); // Включаем UI Image
-                    currentSlotIndex++; // Переходим к следующему слоту
+                    uiImage.sprite = spriteRenderer.sprite; // Устанавливаем спрайт
+                    uiImage.gameObject.SetActive(true); // Включаем UI Image
                 }
             }
         }
