07d6f748f3f75b463cff41c74309e524850ee035
Ryan gosuslug <zahlaminn@yandex.ru>
2024-07-29 15:46:33 +1000

    Add Tools again

---
+++ Building_D_Escape/Assets/Common/Scripts/Teleport.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTeleport : MonoBehaviour
+{
+    public Transform destination; // Точка назначения, куда игрок должен переместиться
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = destination.position; // Перемещение игрока
+        }
+    }
+}
+
