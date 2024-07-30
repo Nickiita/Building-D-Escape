a2161438fd740c80fc72dcc09c14b64b1ceeec72
Nickii <51742504+Nickiita@users.noreply.github.com>
2024-07-30 11:56:34 +1000

    Revert "security go to point"
    This reverts commit 7f08317a590bc1b15948477c0884b9ece7435f68.

--- Building_D_Escape/Assets/Common/Characters/Security/Anim_Security.cs
+++ Building_D_Escape/Assets/Common/Characters/Security/Anim_Security.cs
@@ -5,95 +5,55 @@ using System;
 public class Anim_Security : MonoBehaviour
 {
     private Animator animator;
+
     private Vector3 lastPosition; // Последняя известная позиция NPC
     private int horizontalDirection = 0; // Направление движения по горизонтали: -1 для влево, 1 для вправо, 0 для отсутствия движения
     private int verticalDirection = 0; // Направление движения по вертикали: -1 для вниз, 1 для вверх, 0 для отсутствия движения
     private Vector2 movementInput;
-
-
     void Start()
     {
-                StartCoroutine(CheckEvery200ms());
-
+        lastPosition = transform.position;
     }
-IEnumerator CheckEvery200ms()
-    {
-        while (true)
-        {
-
-            // Здесь ваш код, который должен выполняться каждые 0.2 секунды
-            Debug.Log("Это сообщение выводится каждые 1 секунды");
-
-            yield return new WaitForSeconds(1f);
-        }
-    }
-
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
     void Update()
     {
-        
-        float mov_x = transform.position.x -   lastPosition.x;
-        float mov_y  = transform.position.y -  lastPosition.y;
-        horizontalDirection = 0;
-        verticalDirection = 0;
-         if (Math.Abs(mov_x) >= Math.Abs(mov_y))
+        if (transform.position.x > lastPosition.x)
         {
-            if (mov_x > 0) {
-                horizontalDirection = 1;
-            }else if(mov_x < 0) {
-                horizontalDirection = -1;
-            }else {
-                horizontalDirection = 0;
-            }
-        }else {
-            if (mov_y > 0) {
-                verticalDirection = 1;
-            }else if(mov_y < 0) {
-                verticalDirection = -1;
-            }else {
-                verticalDirection = 0;
-            }
+            horizontalDirection = 1;
+        }
+        else if (transform.position.x < lastPosition.x)
+        {
+            horizontalDirection = -1;
+        }
+        else
+        {
+            horizontalDirection = 0;
         }
 
-        // if (transform.position.x > lastPosition.x)
-        // {
-        //     horizontalDirection = 1;
-        // }
-        // else if (transform.position.x < lastPosition.x)
-        // {
-        //     horizontalDirection = -1;
-        // }
-        // else
-        // {
-        //     horizontalDirection = 0;
-        // }
-
-        // if (transform.position.y > lastPosition.y)
-        // {
-        //     verticalDirection = 1;
-        // }
-        // else if (transform.position.y < lastPosition.y)
-        // {
-        //     verticalDirection = -1;
-        // }
-        // else
-        // {
-        //     verticalDirection = 0;
-        // }
+        if (transform.position.y > lastPosition.y)
+        {
+            verticalDirection = 1;
+        }
+        else if (transform.position.y < lastPosition.y)
+        {
+            verticalDirection = -1;
+        }
+        else
+        {
+            verticalDirection = 0;
+        }
 
 
-        // if (Math.Abs(transform.position.y) > Math.Abs(transform.position.x))
-        // {
-        //     horizontalDirection = 0; 
-        // }else {
-        //     verticalDirection = 0;
+        if (Math.Abs(transform.position.y) > Math.Abs(transform.position.x))
+        {
+            horizontalDirection = 0; 
+        }else {
+            verticalDirection = 0;
 
-        // }
-
-
+        }
 
         lastPosition = transform.position;
 
@@ -102,7 +62,6 @@ IEnumerator CheckEvery200ms()
 
         animator.SetBool("IsMoving", movementInput != Vector2.zero);
 
-        
         animator.SetFloat("MoveX", movementInput.x);
         animator.SetFloat("MoveY", movementInput.y);
     }
--- Building_D_Escape/Assets/Common/Characters/Security/NPSFollow.cs
+++ Building_D_Escape/Assets/Common/Characters/Security/NPSFollow.cs
@@ -1,20 +1,73 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-using System.Collections.Generic;
+// public class NPSFollow : MonoBehaviour
+
+// {
+
+//     public GameObject player;
+//     public float speed;
+
+//     private float distance;
+//     public float followDistance;
+
+
+//     void Start()
+//     {
+
+//     }
+
+// //     void Update(){
+// //         distance =Vector2.Distance(transform.position, player.transform.position);
+// //         Vector2 direction = player.transform.position - transform.position;
+// //         direction.Normalize();
+        
+
+
+// //         if(distance < followDistance) {
+// //             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+// //         }
+// //     }
+// // }
+
+
+
+// void Update()
+// {
+//     distance = Vector2.Distance(transform.position, player.transform.position);
+//     Vector2 direction = player.transform.position - transform.position;
+//     direction.Normalize();
+
+//     // Проверяем, есть ли столкновение с любой из стен
+//     RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, followDistance, LayerMask.GetMask("SolidObjects"));
+//     if (hit.collider != null)
+//     {
+//         // Если есть столкновение, то вычисляем новое направление движения, обходя стену
+//         direction = Vector2.Perpendicular(hit.normal).normalized;
+//     }
+
+//     if (distance < followDistance)
+//     {
+//         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position + (Vector3)direction * speed * Time.deltaTime, speed * Time.deltaTime);
+//     }
+// }
+// }
+
 
 using UnityEngine;
 using UnityEngine.AI;
 using KnightAdventure.Utils;
 using System;
 
-public class EnemyAI : MonoBehaviour
-{
-    private Animator animator;
+public class EnemyAI : MonoBehaviour {
+        private Animator animator;
 
 
     [SerializeField] private State _startingState;
-    // [SerializeField] private float _roamingDistanceMax = 7f;
-    // [SerializeField] private float _roamimgDistanceMin = 3f;
-    // [SerializeField] private float _roamimgTimerMax = 2f;
+    [SerializeField] private float _roamingDistanceMax = 7f;
+    [SerializeField] private float _roamimgDistanceMin = 3f;
+    [SerializeField] private float _roamimgTimerMax = 2f;
 
     [SerializeField] private bool _isChasingEnemy = false;
     [SerializeField] private float _chasingDistance = 4f;
@@ -27,7 +80,7 @@ public class EnemyAI : MonoBehaviour
 
     private NavMeshAgent _navMeshAgent;
     private State _currentState;
-    // private float _roamingTimer;
+    private float _roamingTimer;
     private Vector3 _roamPosition;
     private Vector3 _startingPosition;
 
@@ -41,37 +94,22 @@ public class EnemyAI : MonoBehaviour
     // public event EventHandler OnEnemyAttack;
 
     public GameObject playerObject;
-    public Vector3 playerPosition;
+    public Vector3 playerPosition; 
 
-    public List<Vector3> roamingPoints;
-    private int roamingCurrentIndex = 0;
-    void Start()
-    {
-        roamingPoints = new List<Vector3>
-        {
-            new Vector3(60, 0, 0),
-            new Vector3(60, 40, 0),
-        };
-    }
 
-    public bool IsRunning
-    {
-        get
-        {
-            if (_navMeshAgent.velocity == Vector3.zero)
-            {
+
+    public bool IsRunning {
+        get {
+            if (_navMeshAgent.velocity == Vector3.zero) {
                 return false;
-            }
-            else
-            {
+            } else {
                 return true;
             }
         }
     }
 
 
-    private enum State
-    {
+    private enum State {
         Idle,
         Roaming,
         Chasing,
@@ -79,8 +117,7 @@ public class EnemyAI : MonoBehaviour
         Death
     }
 
-    private void Awake()
-    {
+    private void Awake() {
         animator = GetComponent<Animator>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _navMeshAgent.updateRotation = false;
@@ -92,10 +129,9 @@ public class EnemyAI : MonoBehaviour
     }
 
 
-    private void Update()
-    {
-
-        if (playerObject != null){
+    private void Update() {
+                if (playerObject != null)
+        {
             playerPosition = playerObject.transform.position;
         }
         StateHandler();
@@ -103,17 +139,25 @@ public class EnemyAI : MonoBehaviour
     }
 
 
-    private void StateHandler()
-    {
-        switch (_currentState)
-        {
+    private void StateHandler() {
+        switch (_currentState) {
             case State.Roaming:
-                Roaming();
+                _roamingTimer -= Time.deltaTime;
+                if (_roamingTimer < 0) {
+                    Roaming();
+                    _roamingTimer = _roamimgTimerMax;
+                }
                 CheckCurrentState();
                 break;
             case State.Chasing:
                 ChasingTarget();
                 CheckCurrentState();
+                break;
+            // case State.Attacking:
+            //     AttackingTarget();
+            //     CheckCurrentState();
+            //     break;
+            case State.Death:
                 break;
             default:
             case State.Idle:
@@ -121,42 +165,38 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    private void ChasingTarget()
-    {
+    private void ChasingTarget() {
         _navMeshAgent.SetDestination(playerPosition);
     }
 
-    public float GetRoamingAnimationSpeed()
-    {
+    public float GetRoamingAnimationSpeed() {
         return _navMeshAgent.speed / _roamingSpeed;
     }
 
-    private void CheckCurrentState()
-    {
+    private void CheckCurrentState() {
         float distanceToPlayer = Vector3.Distance(transform.position, playerPosition);
         State newState = State.Roaming;
 
-        if (_isChasingEnemy)
-        {
-            if (distanceToPlayer <= _chasingDistance)
-            {
+        if (_isChasingEnemy) {
+            if (distanceToPlayer <= _chasingDistance) {
                 newState = State.Chasing;
             }
         }
-        if (newState != _currentState)
-        {
-            if (newState == State.Chasing)
-            {
+
+        // if (_isAttackingEnemy) {
+        //     if (distanceToPlayer <= _attackingDistance) {
+        //         newState = State.Attacking;
+        //     }
+        // }
+
+        if (newState != _currentState) {
+            if (newState == State.Chasing) {
                 _navMeshAgent.ResetPath();
                 _navMeshAgent.speed = _chasingSpeed;
-            }
-            else if (newState == State.Roaming)
-            {
-                // _roamingTimer = 0f;
+            } else if (newState == State.Roaming) {
+                _roamingTimer = 0f;
                 _navMeshAgent.speed = _roamingSpeed;
-            }
-            else if (newState == State.Attacking)
-            {
+            } else if (newState == State.Attacking) {
                 _navMeshAgent.ResetPath();
             }
 
@@ -164,11 +204,16 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
+    // private void AttackingTarget() {
+    //     if (Time.time > _nextAttackTime) {
+    //         OnEnemyAttack?.Invoke(this, EventArgs.Empty);
 
-    private void MovementDirectionHandler()
-    {
-        if (Time.time > _nextCheckDirectionTime)
-        {
+    //         _nextAttackTime = Time.time + _attackRate;
+    //     }
+    // }
+
+    private void MovementDirectionHandler() {
+        if (Time.time > _nextCheckDirectionTime) {
 
 
             _lastPosition = transform.position;
@@ -177,28 +222,24 @@ public class EnemyAI : MonoBehaviour
     }
 
 
-    private void Roaming()
-    {
-        // _startingPosition = transform.position;
-        
-
-        int nextRoamingIndex = (roamingCurrentIndex + 1) % roamingPoints.Count;
-        if (Math.Abs(transform.position.x - roamingPoints[nextRoamingIndex].x )< 5 && Math.Abs(transform.position.y - roamingPoints[nextRoamingIndex].y )< 5) {
-            // дошел до точки
-            _roamPosition = GetRoamingPosition();
-        }
+    private void Roaming() {
+        _startingPosition = transform.position;
+        _roamPosition = GetRoamingPosition();
         _navMeshAgent.SetDestination(_roamPosition);
     }
 
 
 
-    private Vector3 GetRoamingPosition()
-    {
-        // return _startingPosition + Utils.GetRandomDir() * UnityEngine.Random.Range(_roamimgDistanceMin, _roamingDistanceMax);
-        Vector3 result = roamingPoints[roamingCurrentIndex];
-        roamingCurrentIndex = (roamingCurrentIndex + 1) % roamingPoints.Count;
-        return result;
+    private Vector3 GetRoamingPosition() {
+        return _startingPosition + Utils.GetRandomDir() * UnityEngine.Random.Range(_roamimgDistanceMin, _roamingDistanceMax);
     }
 
+    // private void ChangeFacingDirection(Vector3 sourcePosition, Vector3 targetPosition) {
+    //     if (sourcePosition.x > targetPosition.x) {
+    //         transform.rotation = Quaternion.Euler(0, -180, 0);
+    //     } else {
+    //         transform.rotation = Quaternion.Euler(0, 0, 0);
+    //     }
+    // }
 
 }
