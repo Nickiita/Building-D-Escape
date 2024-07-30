9aa4d028537df9c1174aded0f03073d536f16e6d
NotTheStas <113777167+NotTheStas@users.noreply.github.com>
2024-07-30 22:33:38 +1000

    Breaking Box Dead

--- Building_D_Escape/Assets/Common/Scripts/Crate.cs
+++ Building_D_Escape/Assets/Common/Scripts/Crate.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class Crate : MonoBehaviour
 {
+    public Transform player;
+    public float interactDistance = 8f;
+    private bool canInteract = false;
+
     void OnMouseDown()
     {
-        if (DisappearOnApproach.canBreakCrates)
+        float distance = Vector3.Distance(transform.position, player.position);
+        canInteract = distance < interactDistance;
+
+        if (DisappearOnApproach.canBreakCrates && canInteract)
         {
             Destroy(gameObject); // Удаляем ящик из сцены
         }
